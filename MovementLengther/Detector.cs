94726ca0879ec5d7a8c658a96dcded90ba5ea0a5
dengbw01 <dengbw01@outlook.com>
2022-03-24 13:03:58 +0800

    Sync

--- MovementLengther/Detector.cs
+++ MovementLengther/Detector.cs
@@ -47,8 +47,8 @@ namespace MovementLengther
             Cv2.CvtColor(background, gray1, ColorConversionCodes.BGR2GRAY);
             Cv2.CvtColor(frame, gray2, ColorConversionCodes.BGR2GRAY);
             Mat diff = rt.T(gray2.Clone());
-            Cv2.MedianBlur(diff, diff, 99);
             Cv2.Absdiff(gray1, gray2, diff);
+            Cv2.MedianBlur(diff, diff, 5);
             Mat diff_thresh = diff.Clone();
             Cv2.Threshold(diff, diff_thresh, 35, 255, ThresholdTypes.Binary);
             Cv2.Blur(diff_thresh, diff_thresh, new Size(8, 8));
@@ -124,81 +124,28 @@ namespace MovementLengther
                 Cv2.MorphologyEx(diff, diff, MorphTypes.Close, kernel, new Point(-1, -1));
                 kernel = Cv2.GetStructuringElement(MorphShapes.Rect, new Size(3, 3), new Point(-1, -1));
                 Cv2.MorphologyEx(diff, diff, MorphTypes.Open, kernel, new Point(-1, -1));
-                Cv2.FindContours(diff, out Point[][] conts, out HierarchyIndex[] h, RetrievalModes.External, ContourApproximationModes.ApproxSimple);
+                Cv2.FindContours(diff, out Point[][] conts, out HierarchyIndex[] h,
+                    RetrievalModes.External, ContourApproximationModes.ApproxSimple);
                 kernel.Dispose();
 
                 List<Rect> targets = new();
 
                 Mat f = frame.Clone();
                 {
-                    //List<Point> p = new List<Point>();
-                    //foreach (var obj in conts)
-                    //{
-                    //    var recb = Cv2.BoundingRect(obj);
-                    //    if (recb.Width * recb.Height >= frame.Width * frame.Height / 3500)
-                    //        p.AddRange(obj);
-                    //}
                     if (conts.Length == 0) continue;
                     var rect = Cv2.BoundingRect(conts.Last());
-                    //if (rect.Contains(lastrect.BottomRight - lastrect.TopLeft)) continue;
                     var csize = (rect & lastrect).Size;
                     var coll = csize.Height * csize.Width;
                     if (coll > (rect.Width * rect.Height / 2) ||
                         coll > (lastrect.Width * lastrect.Height / 2))
                         continue;
                     if (rect.X == 0 || rect.Y == 0) continue;
-                    //Cv2.Rectangle(f, rect, Scalar.Red);
-
-                    /*
-                    bool changed = false;
-                    do
-                    {
-                        List<Rect> ftargets = new();
-                        int i = 0, j = 0;
-                        changed = false;
-                        foreach (var target1 in targets)
-                        {
-                            for (j = i; j < targets.Count; j++)
-                            {
-                                var target2 = targets[j];
-                                if (!target1.Equals(target2))
-                                {
-                                    var dist = (target1.Center() - target2.Center()).Distance();
-                                    if (dist < target1.Size.ConnerDist() ||
-                                        dist < target2.Size.ConnerDist())
-                                    {
-                                        ftargets.Add(target1 | target2);
-                                        changed = true;
-                                    }
-                                    else
-                                    {
-                                        //ftargets.Add(target1);
-                                        //changed = false;
-                                    }
-                                }
-                                //if (j == targets.Count - 1)
-                                //{
-                                //    ftargets.Add(target1);
-                                //    changed = false;
-                                //}
-                            }
-                            i++;
-                        }
-                        targets = ftargets.Clone();
-                    } while (changed);
-                    */
                     Rect target = rect;
                     var refpoint = Center(target);
                     Cv2.Rectangle(f, target, Scalar.Red, 2);
                     Cv2.DrawMarker(f, refpoint, Scalar.Red);
-                    //if (Leftend.Hit)
-                    //{
                     Cv2.DrawMarker(f, Leftend.Position, Scalar.Blue, thickness: 2);
-                    //}
-                    //if (Rightend.Hit)
-                    //{
                     Cv2.DrawMarker(f, Rightend.Position, Scalar.Green, thickness: 2);
-                    //}
                     OnMovement?.Invoke(Center(rect));
                     if (!inited)
                     {
@@ -218,25 +165,20 @@ namespace MovementLengther
                         Leftend.Timestamp = DateTime.Now;
                         OnLeftHit?.Invoke(refpoint);
                         watchdog = 0;
-                        RLock = false;
                     }
                     else
                     if (refpoint.X >= Rightend.Position.X)
                     {
-                        //if (!RLock)
-                        {
-                            Rightend.Hit = true;
-                            Rightend.Position = refpoint;
-                            Rightend.Timestamp = DateTime.Now;
-                            OnRightHit?.Invoke(refpoint);
-                            watchdog = 0;
-                        }
+                        Rightend.Hit = true;
+                        Rightend.Position = refpoint;
+                        Rightend.Timestamp = DateTime.Now;
+                        OnRightHit?.Invoke(refpoint);
+                        watchdog = 0;
                     }
                     else
                     {
                         if (Leftend.Hit && Rightend.Hit)//半周期测量完成
                         {
-                            RLock = true;
                             var item = new ResultPair()
                             {
                                 MiliDuration = Math.Abs((Leftend.Timestamp - Rightend.Timestamp).TotalMilliseconds),
